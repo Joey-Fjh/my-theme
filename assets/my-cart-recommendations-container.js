6c84f3cb6daa56f2a44c5d53f7a4aeb11d611879
Joey <1834879843@qq.com>
2025-09-29 09:59:48 +0800

    上传最新的

--- assets/my-cart-recommendations-container.js
+++ assets/my-cart-recommendations-container.js
@@ -1,9 +1,9 @@
-var r=Object.defineProperty;var a=(t,e,s)=>e in t?r(t,e,{enumerable:!0,configurable:!0,writable:!0,value:s}):t[e]=s;var o=(t,e,s)=>a(t,typeof e!="symbol"?e+"":e,s);import{i as c,a as i,x as l}from"./lit-element.js";import{S as d}from"./swiper.js";class n extends c{loadContent(){}connectedCallback(){super.connectedCallback(),new d(".swiper",{slidesPerView:"auto",spaceBetween:18,freeMode:!0})}render(){return l`
+var a=Object.defineProperty;var n=(t,e,s)=>e in t?a(t,e,{enumerable:!0,configurable:!0,writable:!0,value:s}):t[e]=s;var r=(t,e,s)=>n(t,typeof e!="symbol"?e+"":e,s);import{i as c,a as i,x as l}from"./lit-element.js";import{S as d}from"./swiper.js";class o extends c{connectedCallback(){super.connectedCallback(),new d(".swiper",{slidesPerView:"auto",spaceBetween:18,freeMode:!0})}render(){return l`
             <slot></slot>
-        `}}o(n,"styles",[i`
+        `}}r(o,"styles",[i`
             :host{
                 display: block;
                 width: 100%;
                 flex:1;
             }
-        `]);customElements.define("my-cart-recommendations-container",n);
+        `]);customElements.define("my-cart-recommendations-container",o);
--- assets/my-productDetail-container.js
+++
@@ -1,3 +0,0 @@
-import{i as t,x as e}from"./lit-element.js";class r extends t{constructor(){super()}render(){return e`
-            <slot></slot>
-        `}}customElements.define("product-detail",r);
