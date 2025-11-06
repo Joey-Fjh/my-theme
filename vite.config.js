fbe06d21ae0217546cccc106be18cbefa6fcd793
Joey <1834879843@qq.com>
2025-11-06 16:31:45 +0800

    tj

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,20 @@ import postcss from './postcss.config.js';
 function getEntries(type,pattern){
     const glob = require('glob');
     const entries = {};
-
     const options = {};
+
     if(type != 'js'){
-        options.ignore = [
-            'src/components/**'
-        ]
+      options.ignore = [
+        'src/components/**',
+        'src/tools/*'
+      ]
     }
 
     const files = glob.sync(`${pattern}.${type}`,options);
 
     files.forEach(file => {
-        const name = path.basename(file, path.extname(file));
-        entries[name] = path.resolve(__dirname,file);
+      const name = path.basename(file, path.extname(file));
+      entries[name] = path.resolve(__dirname,file);
     });
 
     return entries;
@@ -30,7 +31,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         ...getEntries('js','src/components/*'),
-        ...getEntries('{css,scss}','src/**/*')
+        ...getEntries('{css,scss}','src/**/*'),
+        ...getEntries('js','src/tools/*')
       },
       output: {
         entryFileNames: '[name].js',
