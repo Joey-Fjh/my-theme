b2f98c50a0b9c43b1abf682416fb3cce62582b7f
Joey <1834879843@qq.com>
2025-11-12 11:43:21 +0800

    1

--- src/components/my-newSletter-popup.js
+++ src/components/my-newSletter-popup.js
@@ -1,12 +1,30 @@
 import {html, LitElement} from 'lit';
 
 class MyNewSletterPopup extends LitElement { 
+    static properties = {
+        displayMode: {type: String},
+        showInHome: { type: Boolean },
+        showForVisitor: { type: Boolean },
+    }
+
     constructor() {
         super();
+
+        this.open = false;
+
+        this.displayMode = null;
+        this.showInHome = true;
+        this.showForVisitor = true;
     }
 
     connectedCallback() {
         super.connectedCallback();
+
+        this.show();
+    }
+
+    show(){
+        console.log(this.displayMode,'---',this.showInHome,'---',this.showForVisitor);
     }
 
     render() { 
