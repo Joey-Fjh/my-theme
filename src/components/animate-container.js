cf41292363ad0d72bee5199103d34b11aa758bbe
Joey <1834879843@qq.com>
2025-11-18 13:52:34 +0800

    1

--- src/components/animate-container.js
+++ src/components/animate-container.js
@@ -28,12 +28,13 @@ export class AnimateUtilities {
 }
 
 export class AnimateContainer extends LitElement { 
-    static styles = css`
+    static styles = [css`
             :host {
                 width: 100%;
                 height: 100%;
             }
         `
+    ]
 
     static get properties() {
         return {
