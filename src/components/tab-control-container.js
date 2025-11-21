b42a745512d59b3baceffb4449f7963ee90b9737
Joey <1834879843@qq.com>
2025-11-21 14:38:56 +0800

    1

--- src/components/tab-control-container.js
+++ src/components/tab-control-container.js
@@ -11,6 +11,8 @@ class TabControl extends LitElement {
         this.navContainer = this.querySelector('[data-tab-nav]');
         this.panels = this.querySelectorAll('.tab-panel');
 
+        this.buildTabs();
+
         this.panels.forEach((panel, index) => { 
             const title = panel.getAttribute('data-title');
 
@@ -24,29 +26,81 @@ class TabControl extends LitElement {
         });
 
         this._handleClick = this.handleClick.bind(this);
+        this._handleSlotChange = this.handleSlotChange.bind(this);
+
         this.navContainer.addEventListener('click',this._handleClick);
+        this.shadowRoot?.querySelector("slot")?.addEventListener("slotchange", this._handleSlotChange);
+
+        // init active tab
+        if (this.panels.length > 0) {
+            this.activateTab(0);
+        }
+    }
+
+    buildTabs(){
+        this.navContainer.innerHTML = ''; // clear old tab titles
+
+        this.panels.forEach((panel, index) => { 
+            const title = panel.getAttribute('data-title');
+
+            const div = document.createElement('div');
+            
+            div.className = 'tab-title';
+            div.innerText = title;
+            div.setAttribute('data-tab-index', index);
+            
+            panel.setAttribute('data-tab-index', index);
+
+            this.navContainer.appendChild(div);
+        });
+    }
+
+    activeTab(index){
+        this.panels.forEach(panel => {
+            if(!panel.classList.contains("hidden")) panel.classList.add("hidden");
+            panel.classList.remove("active");
+        });
+
+        for(const title of this.navContainer.children){
+            title.classList.remove("active");
+        }
+
+        const title = this.navContainer.children[index];
+        const panel = this.panels[index];
+
+        if(title & panel){
+            title.classList.add("active");
+            panel.classList.remove("hidden");
+            panel.classList.add("active");
+        } 
     }
 
     handleClick(e) {
         const target = e.target.closest('[data-tab-index]');
         if(!target) return;
 
-        this.panels.forEach(panel => {
-            panel.classList.remove("active");
-        });
+        const index = Number(target.getAttribute('data-tab-index'));
+        this.activeTab(index);
+    }
 
-        for (const title of this.navContainer.children) {
-            title.classList.remove("active");
+    handleSlotChange(){
+        const oldActiveIndex = [...this.panels].findIndex(panel => panel.classList.contains('active'));
+        
+        this.buildTabs();
+        
+        if(!this.panels[oldActiveIndex]){
+            const newIndex = Math.max(0,oldActiveIndex-1);
+            this.activeTab(newIndex);
+        }else{
+            this.activeTab(oldActiveIndex);
         }
-
-        target.classList.add("active");
-        this.panels[target.getAttribute("data-tab-index")].classList.add("active");
     }
 
     disconnectedCallback() {
         super.disconnectedCallback();
 
         this.navContainer.removeEventListener('click',this._handleClick);
+        this.shadowRoot?.querySelector("slot")?.removeEventListener("slotchange", this._handleSlotChange);
     }
 
     render() {
